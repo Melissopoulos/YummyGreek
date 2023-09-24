d1887c9e586abddf8015b3f2ff3f5d8f7f9eb036
George Melissopoulos <melissopoulos.geo@gmail.com>
2023-09-24 21:29:35 +0300

    implementation of DishesList

--- yummy-greek/src/components/DishesList.js
+++ yummy-greek/src/components/DishesList.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Dish from "./Dish";
+import Loading from "./Loading";
+const url = "http://localhost:8000/dishes/";
 
 const DishesList = () => {
-  return <h2>DishesList</h2>;
+  const [loading, setLoading] = useState(false);
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setDishes(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (dishes.length < 1) {
+    return (
+      <h2 className="section-title">no dishes matched your search criteria</h2>
+    );
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">dishes</h2>
+      <div className="cocktails-center">
+        {dishes.map((dish) => {
+          return <Dish key={dish.id} {...dish} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default DishesList;
